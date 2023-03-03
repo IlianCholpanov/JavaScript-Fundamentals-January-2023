9c8469b67793f79dceeceaa137dee516b24f45c3
Ilian Cholpanov <123170166+IlianCholpanov@users.noreply.github.com>
2023-03-03 14:39:27 +0200

    Delete demo.js

--- demo.js
+++
@@ -1,7 +0,0 @@
-function demo(input){
-    console.log(!input.includes('coco')); // returns true || false
-
-    console.log(input.indexOf('denis')); // returns position of index
-}
-demo(['ilko', 'denis', 'coco'])
-
